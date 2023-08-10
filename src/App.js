5b2a5eaffa8f89adde815286cb346a09feb61909
Aadarsh Kumar <aadarsh.kumar.21@mountblue.tech>
2023-08-10 11:33:25 +0530

    upload Score Board project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CricketScore from './component/CricketScore';
+
+function App() {
+  return (
+    <div className="App">
+      <CricketScore/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CricketScore.js
@@ -0,0 +1,43 @@
+import React,{useEffect, useState} from 'react'
+import '../styles/CricketScore.css';
+function CricketScore() {
+    const [score,setScore]=useState(0);
+    const [wicket,setWicket]=useState(0);
+    const [over,setOver]=useState(0);
+    const [count,setCount]=useState(0);
+
+    useEffect(()=>{
+     if(count===6){
+        setOver(over+1);
+        setCount(0);
+     }
+    },[count])
+    const addScore=(num)=>{
+        if(num!=='wicket'){
+            setScore(score+num);
+            setCount(count+1);
+        }else{
+            setWicket(wicket + 1);
+            setCount(count+1);
+        }
+        
+    }
+  return (
+    <div className='container'>
+        <h2>Score:{score}/{wicket}</h2>
+        <h2>Over:{over}</h2>
+      <div className="btn">
+      <button onClick={()=>addScore(0)}>0</button>
+         <button onClick={()=>addScore(1)}>1</button>
+         <button onClick={()=>addScore(2)}>2</button>
+         <button onClick={()=>addScore(3)}>3</button>
+         <button onClick={()=>addScore(4)}>4</button>
+         <button onClick={()=>addScore(5)}>5</button>
+         <button onClick={()=>addScore(6)}>6</button>
+         <button onClick={()=>addScore('wicket')}>Wicket</button>
+      </div>
+    </div>
+  )
+}
+
+export default CricketScore
